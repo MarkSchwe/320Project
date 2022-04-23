b533425e50d3e3e596e084fe7051e4ac3cbf0101
MarkSchwe <91101294+MarkSchwe@users.noreply.github.com>
2022-04-23 12:04:06 -0400

    Create 320Project.cpp

---
+++ 320Project.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "prediction.h"
+using namespace std;
+
+
+int main(){
+    srand(time(0));
+vector<data> d;
+ifstream file;
+file.open("TestFile3.dat");
+double t1,t2;
+while(!file.eof()){
+    file >> t1;
+    file >> t2;
+d.push_back(data(t1,t2));
+}
+file.close();
+/*
+for(int i = 0; i < d.size(); i++){
+    cout << d[i] << endl;
+}
+*/
+datavg avg;
+avg.setavgs(d);
+cout << avg << endl;
+prediction prod;
+prod.insertDat(avg);
+cout << "How many years ahead would you like the computer to predict? ";
+double yrs;
+cin >> yrs;
+prod.predict(yrs,d);
+cout << prod << endl;
+    return 0;
+}
